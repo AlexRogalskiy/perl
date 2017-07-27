01c31db75ae243c78147eee6487cd7fd6e3c7c0b
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-07-27 23:52:53 +0300

    Changeset to Init Repo commit

---
+++ case_01.pl
@@ -0,0 +1,13 @@
+#!C:\xampp\perl\bin\perl -w
+#/usr/bin/perl -w
+use CGI::Carp qw(fatalsToBrowser);
+print "Content-type: text/html\n\n";
+
+print "<html>\n";
+print "<head>\n";
+print "<title>Test</title>\n";
+print "</head>\n";
+print "<body>\n";
+print "Test\n";
+print "</body>\n";
+print "</html>\n";
