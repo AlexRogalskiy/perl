c82699f479426f708f3f6d03dc3b3f07cc9ae492
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-07-30 12:56:37 +0300

    Changeset to Perl expressions

--- case_08.pl
+++ case_08.pl
@@ -55,11 +55,63 @@ for (my $i = 0; $i < @mass; $i++) {
 	print "$mass[$i]", "\n";
 }
 
+foreach (@mass) {
+	print "= $_\n";
+}
+
+foreach (1..10) {
+	print "$_\n";
+}
+
+for (my $i = 0; $i < 10; $i++) {
+	next if($i>4 && $i<11);
+	print "$i\n";
+}
+
+for (my $i = 0; $i < 10; $i++) {
+	last if($i>5);
+	print "*$i\n";
+}
+
+$i=1;
+while ($i<10) {
+	if ($i<15) {
+		print "$i\n";
+		$i++;
+		redo;
+	}
+}
+
+$i=1;
+BLOCK: {
+	last BLOCK if($i>15);
+	print "$i\n";
+	$i++;
+	redo BLOCK;
+}
+
+sub f_sum() {
+	($x, $y) = @_;
+	$x = 1 unless (defined($x));
+	$y = 2 unless (defined($y));
+	return $x + $y;
+}
+#$var = f_sum(4, 7);
+
+sub f_factorial {
+	my $x = shift();
+	if ($x == 0 || $x == 1) {
+		return 1;
+	} else {
+		return $x * f_factorial($x - 1);
+	}
+}
+
 #/^[0-9]+$/
 #qr/<(.+)>(.*)<\/\1>/;
 #qr/<s1>(.*?)<\/s1>/;
 
-print $@ if(defined($a));
+#print $@ if(defined($a));
 
 #@_
 sub f_pass_gen {
@@ -76,10 +128,96 @@ sub f_pass_gen {
 print f_pass_gen(4);
 
 @mass = gmtime(time());
-@mass2 = localtime(time());
+@mass = localtime(time());
 
 use POSIX;
 use locale;
 setlocale(LC_ALL, 'ru_RU.CP1251');
 
 print strftime("\nToday %A %d.%m.%Y %H:%M:%S", localtime());
+
+
+sub f_function {
+	my $mass_link = shift();
+	foreach my $var (@$mass_link) {
+		$var += 5;
+	}
+}
+@mass = (1..10);
+f_function(\@mass);
+foreach my $var (@mass) {
+	print "\n--> $var\n";
+}
+
+sub f_function2 {
+	my $hash_link = shift();
+	foreach my $var (keys(%$hash_link)) {
+		$$hash_link{$var} += 5;
+	}
+}
+%hash = ("One" => 1, "Two" => 2, "Three" => 3);
+f_function2(\%hash);
+while (($key, $value) = each(%hash)) {
+	print "\n$key => $value\n";
+}
+
+sub f_sum2 {
+	my $sum = 0;
+	for (my $i = 0; $i < @_; $i++) {
+		$sum += $_[$i];
+	}
+	return $sum;
+}
+
+print "Scalar context: " . f_func() . "\n";
+@mass2 = f_func();
+print "List context: @mass2";
+
+sub f_func {
+	my @mass = (1..10);
+	if(wantarray()) {
+		return @mass;
+	} else {
+		return scalar(@mass);
+	}
+}
+
+no strict 'refs';
+
+our $var1 = 10;
+my $link1 = "var1";
+print "\n\$var1 = $$link1";
+print "\n\$var1 = ${$link1}";
+
+our $var2 = 5;
+my $link2 = "var2";
+print "\n\$var2 = $$link2\n";
+
+
+my $link3 = \$var1;
+print ref($link3); #SCALAR, REF, ARRAY, HASH, CODE
+
+my $link4 = \&f_print;
+&$link4();
+$link4->();
+
+sub f_print {
+	print "\nTest\n";
+}
+
+sub f_print2 {
+	my $txt1 = shift();
+	return sub {
+		my $txt2 = shift();
+		print $txt1, $txt2, "\n";
+	}
+}
+
+my $link5 = &f_print2('Test');
+$link5->('e');
+&$link5('t');
+
+sub f_sum3 {
+	return ($_[0] + $_[1]);
+}
+print "2 + 2 = ${\f_sum3(2, 2)}";
---
+++ case_09.pl
@@ -0,0 +1,135 @@
+#!C:\xampp\perl\bin\perl -w
+#/usr/bin/perl -w
+
+use strict;
+
+#my @mass_ref = (\@mass, \@mass2);
+my @mass_ref = ([], []);
+
+foreach my $row (@mass_ref) {
+	foreach my $item (@$row) {
+		print "$item\n";
+	}
+}
+
+$mass_ref[2] = ['1', '2'];
+my @m = ("One", "Two");
+@{$mass_ref[3]} = @m;
+
+for (my $i = 0; $i < @mass_ref; $i++) {
+	for (my $j = 0; $j < @{$mass_ref[$i]}; $j++) {
+		print "\n> $mass_ref[$i][$j]\n";
+	}
+}
+
+my $link = [[], []];
+foreach my $row (@$link) {
+	foreach my $item (@$row) {
+		print "$item ";
+	}
+	print "\n";
+}
+
+$link->[2] = [];
+my @mass = ("", "");
+$link->[3] = [@mass];
+
+for (my $i = 0; $i < @$link; $i++) {
+	for (my $j = 0; $j < @{$link->[$i]}; $j++) {
+		print $link->[$i][$j], " ";
+	}
+	print "\n";
+}
+
+my %tag = (
+	"p" => {},
+	"img" => {}
+);
+
+#$tag{"img"}{"src"};
+#$tag{"img"}->{"src"};
+#${$tag{"img"}}{"src"};
+$tag{"a"} = {};
+
+foreach my $var1 (sort(keys(%tag))) {
+	print "$var1: ";
+	foreach my $var2 (sort(keys(%{$tag{"$var1"}}))) {
+		print "$var2 -> $tag{$var1}{$var2}";
+	}
+	print "\n";
+}
+
+my @car = ({"m" => "m"}, {"g" => "g"});
+#$car[0]{""};
+#$car[0]->{""};
+#${$car[0]}{""};
+
+foreach my $var (@car) {
+	foreach my $key (sort(keys(%$var))) {
+		print "<> $key => $var->{$key}";
+	}
+	print "\n";
+}
+
+push(@car, {"c" => "c"});
+
+for (my $i = 0; $i < @car; $i++) {
+	print "[$i]";
+	foreach my $key (sort(keys(%{$car[$i]}))) {
+		print "= $key => $car[$i]{$key}";
+	}
+	print "\n";
+}
+
+my %word = ("a" => [], "b" => []);
+#$word{"a"}[2];
+#$word{"a"}->[2];
+#${$word{"a"}}[2];
+
+$word{"c"} = [];
+
+foreach my $key (sort(keys(%word))) {
+	print "$key => ";
+	for (my $i = 0; $i < @{$word{$key}}; $i++) {
+		print "$word{$key}[$i]\n";
+	}
+}
+
+my %switch = ("a" => \&f_sub1, "b" => \&f_sub2);
+my $str = "a";
+if($str eq "") {
+	exit 1;
+} elsif(exists($switch{$str})) {
+	$switch{$str}->();
+} else {
+	print "not found\n";
+}
+
+sub f_sub1 {
+	print "f_sub1\n";
+	return 0;
+}
+
+sub f_sub2 {
+	print "f_sub2\n";
+	return 0;
+}
+
+#if(ref($tag[$i] eq 'HASH')) {}
+
+#my $var1 = *var{SCALAR}; //ARRAY, HASH, CODE
+our $str1 = "11";
+my $link1 = \*str1;
+print ref($link1); ##GLOB
+print "\n".$$$link1;
+
+our $PI = 3.14;
+my $pi = *PI{SCALAR};
+print "\n".$$pi;
+
+#no strict "subs";
+*p = \3.14;
+print "\n".${*p};
+
+print "\nPackage name: " . __PACKAGE__ . "\n";
+print &main::f_sub1;
